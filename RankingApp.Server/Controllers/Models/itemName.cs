c09f86cc9ebbb57150f47f0c422c4db45dff2985
Matthew Lowe <matthewjlowe11@gmail.com>
2025-05-29 11:37:49 -0400

    Add project files.

---
+++ RankingApp.Server/Controllers/Models/itemName.cs
@@ -0,0 +1,15 @@
+namesspace RankingApp.Models
+{
+    public class ItemModel
+    {
+       public int Id { get; set; }
+
+       public string title { get; set; }
+
+       public string ImageId { get; set; }
+       
+       public int Ranking { get; set; }
+
+       public int ItemType { get; set; }
+}
+}
