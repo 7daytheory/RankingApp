65dbd9111cce3cfc0349ea88e393774e27d7ea72
Matthew Lowe <matthewjlowe11@gmail.com>
2025-06-03 10:35:30 -0400

    Remove tailwind - add custom css for navigation

---
+++ rankingapp.client/src/NavMenu.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './NavMenu.css';
+
+export class NavMenu extends Component {
+    static displayName = NavMenu.name;
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    render() {
+        return (
+            <header>
+                <Navbar className="custom-navbar" container light>
+                    <NavbarBrand tag={Link} to="/">RankingApp</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNavbar} className="custom-toggler" />
+                            <Collapse className="custom-collapse" isOpen={!this.state.collapsed} navbar>
+                                <ul className="custom-nav">
+                                    <NavItem>
+                                        <NavLink tag={Link} className="custom-link" to="/">Home</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} className="custom-link" to="/fetch-data">Fetch data</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} className="custom-link" to="/rank-items">Rank Items</NavLink>
+                                    </NavItem>
+                                </ul>
+                            </Collapse>
+                    </Navbar>
+            </header>
+        );
+    }
+}
