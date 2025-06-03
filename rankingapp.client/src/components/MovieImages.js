2a216fef433a8a4ef956c0d0779b1e83f49ec045
Matthew Lowe <matthewjlowe11@gmail.com>
2025-06-03 11:16:10 -0400

    Add Superman4 poster that was missing

--- rankingapp.client/src/components/MovieImages.js
+++ rankingapp.client/src/components/MovieImages.js
@@ -7,6 +7,7 @@ import RearWindow from '../images/movies/RearWindow.png';
 import RoadHouse from '../images/movies/RoadHouse.png';
 import Shawshank from '../images/movies/Shawshank.png';
 import StarTrack4 from '../images/movies/StarTreck4.png';
+import Superman4 from '../images/movies/Superman4.jpg';
 
 const MovieImageArr = [
     { id: 1, image: Godfather },
@@ -18,6 +19,7 @@ const MovieImageArr = [
     { id: 7, image: RoadHouse },
     { id: 8, image: Shawshank },
     { id: 9, image: StarTrack4 },
+    { id: 10, image: Superman4 },
 ];
 
 export default MovieImageArr;
