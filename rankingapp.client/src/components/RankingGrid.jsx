c0b7c375a1d6073c33f090df27341ed5dc91feb1
Matthew Lowe <matthewjlowe11@gmail.com>
2025-06-04 11:58:22 -0400

    Setup RankingGrid page, create variables to store rankings

---
+++ rankingapp.client/src/components/RankingGrid.jsx
@@ -0,0 +1,18 @@
+const RankingGrid = ({ items, imgArr }) => {
+    
+    const rankingGrid = [];
+    const cellCollectionTop = [];
+    const cellCollectionMiddle = [];
+    const cellCollectionBottom = [];
+    const cellCollectionWorst = [];
+
+    return (
+        <div className="rankings">
+              
+        </div>
+    )
+
+
+})
+
+export default RankingGrid;
